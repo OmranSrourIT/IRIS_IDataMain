13110d493ef300e1ed4e014631411bc32bf1dbb6
omran.srour@polaris-tek.com <omran.srour@polaris-tek.com>
2022-10-08 17:03:23 +0300

    latest IDATA IRIS 10082022

--- IRIS_IDataMain/App_Start/WebApiConfig.cs
+++ IRIS_IDataMain/App_Start/WebApiConfig.cs
@@ -18,7 +18,7 @@ namespace IRIS_IDataMain
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                 routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
--- IRIS_IDataMain/Controllers/IRISIDController.cs
+++ IRIS_IDataMain/Controllers/IRISIDController.cs
@@ -14,12 +14,8 @@ namespace IRIS_IDataMain.Controllers
     {
 
 
-        ApiResult oApi = new ApiResult();
-        // GET api/values
-        //https://localhost:44316/api/IRISID/PassImageIRIS_IDATA
-        //https://192.168.88.232:44316/api/IRISID/PassImageIRIS_IDATA
-        //http://localhost/IRIS_IDataMain/api/IRISID/PassImageIRIS_IDATA
         [System.Web.Http.HttpPost]
+
         public HttpResponseMessage PassImageIRIS_IDATA(List<string> eyes)
         {
             if (eyes == null || eyes.Count == 0 || eyes.Count == 1)
@@ -38,10 +34,10 @@ namespace IRIS_IDataMain.Controllers
             List<ResponseImage> ResImage = new List<ResponseImage>();
             ResponseImage OBJ_IMG = new ResponseImage();
 
-           
-           
 
-            if (eyes[0] != "" && eyes[1] !="")
+
+
+            if (eyes[0] != "" && eyes[1] != "")
             {
                 liftEye = eyes[0];
                 rightEye = eyes[1];
@@ -50,7 +46,7 @@ namespace IRIS_IDataMain.Controllers
             }
             else if (eyes[0] == "" && eyes[1] != "")
             {
-                rightEye = eyes[1]; 
+                rightEye = eyes[1];
                 qulityRight = irisMethods.checkQulity(rightEye);
                 qulityLift = 110; //The Eyes Is Missing
             }
@@ -70,31 +66,91 @@ namespace IRIS_IDataMain.Controllers
             OBJ_IMG.ImageRight = eyes[1];
             OBJ_IMG.ImageQuailtyLeft = qulityLift.ToString();
             OBJ_IMG.ImageQuailtyRight = qulityRight.ToString();
-      
+
             ResImage.Add(OBJ_IMG);
 
 
             if (qulityLift > 75 && qulityRight > 75)
-            { 
+            {
                 isGoodQulity = true;
                 OBJ_IMG.MessageQuailty = "Success,High quality";
                 OBJ_IMG.ResultQuailty = true;
             }
             else
-            { 
+            {
                 isGoodQulity = false;
                 OBJ_IMG.MessageQuailty = "Fialed,Low Quality";
                 OBJ_IMG.ResultQuailty = false;
             }
 
 
-            return Request.CreateResponse(HttpStatusCode.OK,  ResImage.ToList(), Configuration.Formatters.JsonFormatter);
+            return Request.CreateResponse(HttpStatusCode.OK, ResImage.ToList(), Configuration.Formatters.JsonFormatter);
         }
 
-        ////https://192.168.88.232:44316/api/IRISID/PassImageIRIS_IDATA1
-        [System.Web.Http.HttpGet]
-        public HttpResponseMessage PassImageIRIS_IDATA1()
+        [System.Web.Http.HttpPost]
+        public HttpResponseMessage VerificationIRISIDATA(List<string> IistEyesPostion)
         {
+
+            string LiftEyeFromSytem = "";
+            string RightEyeFromSytem = "";
+            string LiftEyeFromDevice = "";
+            string RightEyeFromDevice = "";
+            var MatchsCorrect = false; 
+            IrisMethods irisMethods = new IrisMethods();
+
+
+            try
+            {
+                if (IistEyesPostion == null || IistEyesPostion.Count == 0 || IistEyesPostion.Count == 1)
+                {
+                    return Request.CreateResponse(HttpStatusCode.Forbidden, "null eyes", Configuration.Formatters.JsonFormatter);
+                }
+                if (IistEyesPostion[2] != "" && IistEyesPostion[3] != "")
+                {
+                    LiftEyeFromSytem = IistEyesPostion[0];
+                    RightEyeFromSytem = IistEyesPostion[1];
+
+                    LiftEyeFromDevice = IistEyesPostion[2];
+                    RightEyeFromDevice = IistEyesPostion[3];
+
+
+                    MatchsCorrect = irisMethods.VerifyMatchesImageSIRIS(RightEyeFromSytem, RightEyeFromDevice);
+
+                }
+                else if (IistEyesPostion[2] == "" && IistEyesPostion[3] != "")
+                {
+                    RightEyeFromSytem = IistEyesPostion[1];
+                    RightEyeFromDevice = IistEyesPostion[3];
+                    MatchsCorrect = irisMethods.VerifyMatchesImageSIRIS(RightEyeFromSytem, RightEyeFromDevice);
+                    //   EyesLift = The Eyes Is Missing
+                }
+                else if (IistEyesPostion[2] != "" && IistEyesPostion[3] == "")
+                {
+                    LiftEyeFromSytem = IistEyesPostion[0];
+                    LiftEyeFromDevice = IistEyesPostion[2];
+                    MatchsCorrect = irisMethods.VerifyMatchesImageSIRIS(LiftEyeFromSytem,  LiftEyeFromDevice);
+                    //EyesRight = The Eyes Right Is Missing
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, MatchsCorrect, Configuration.Formatters.JsonFormatter);
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, "Error occurred" + ex.Message, Configuration.Formatters.JsonFormatter);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, MatchsCorrect, Configuration.Formatters.JsonFormatter);
+        }
+
+        [System.Web.Http.HttpGet]
+        public HttpResponseMessage PassOmran(int id)
+        {
+            var ddd = id;
 
             return Request.CreateResponse(HttpStatusCode.OK, "Omran", Configuration.Formatters.JsonFormatter);
 
